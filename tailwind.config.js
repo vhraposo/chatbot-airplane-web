a3da5be4512887f38b034f160473acdcb35e33fb
vhraposo <thevhup@gmail.com>
2024-03-08 17:05:59 -0300

    style: add secondary text and border color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,12 @@ module.exports = {
       colors: {
         'primary-color': 'var(--primary-color)',
         'foreground-primary': 'var(--text-primary)',
+        'foreground-secondary': 'var(--text-secondary)',
         'text-bold': 'var(--text-bold)',
         'online-color': 'var(--online-color)',
         'primarybg': 'var(--bg-primary)',
         'secondarybg': 'var(--bg-secondary)',
+        'border-color': 'var(--border-color)',
       }      
     },
   },
