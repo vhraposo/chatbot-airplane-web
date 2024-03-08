df432514099907e26bd121e4d496f5925be257cf
vhraposo <thevhup@gmail.com>
2024-03-08 17:07:31 -0300

    feat: add header and chat insights to chat component

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { ChatInsightsComponent } from '../../components/chat-insights/chat-insights.component';
+import { ArrowLeftComponent } from '../../icons/arrow-left/arrow-left.component';
+import { InterrogationComponent } from '../../icons/interrogation/interrogation.component';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [
+    ArrowLeftComponent,
+    InterrogationComponent,
+    ChatInsightsComponent
+  ],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent {
-
+  
 }
