cdaa942eb2fe264d3b7afa9661c0ee68bf0d06b8
vhraposo <thevhup@gmail.com>
2024-03-08 00:35:12 -0300

    feat: add chat route to app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
+import { ChatComponent } from './pages/chat/chat.component';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
+    },
+    {
+        path: 'chat',
+        component: ChatComponent
     }
 ];
