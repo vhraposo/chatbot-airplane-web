16929cb578ab41f80bc0b94a006e1c9cfea06e65
vhraposo <thevhup@gmail.com>
2024-03-08 00:19:53 -0300

    style: update color variables in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,11 @@ module.exports = {
     extend: {
       colors: {
         'primary-color': 'var(--primary-color)',
-        'text-primary': 'var(--text-primary)',
+        'foreground-primary': 'var(--text-primary)',
         'text-bold': 'var(--text-bold)',
         'online-color': 'var(--online-color)',
-        'bg-primary': 'var(--bg-primary)',
-        'bg-secondary': 'var(--bg-secondary)',
+        'primarybg': 'var(--bg-primary)',
+        'secondarybg': 'var(--bg-secondary)',
       }      
     },
   },
