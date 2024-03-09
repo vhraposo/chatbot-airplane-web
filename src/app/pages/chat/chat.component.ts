86d604edfe7d3ac6a18a66622da58a824bf639ba
vhraposo <thevhup@gmail.com>
2024-03-08 21:38:31 -0300

    feat: add MessageService and update local storage

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -4,6 +4,7 @@ import { ChatDialogComponent } from '../../components/chat-dialog/chat-dialog.co
 import { ChatInsightsComponent } from '../../components/chat-insights/chat-insights.component';
 import { ArrowLeftComponent } from '../../icons/arrow-left/arrow-left.component';
 import { InterrogationComponent } from '../../icons/interrogation/interrogation.component';
+import { MessageService } from '../../services/message.service';
 import { Message } from '../../types/message.type';
 
 @Component({
@@ -16,15 +17,39 @@ import { Message } from '../../types/message.type';
     ChatInsightsComponent,
     ChatDialogComponent
   ],
+  providers: [
+    MessageService
+  ],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent {
-  messages: Message[] = []
+  messages: Message[] = JSON.parse(localStorage.getItem('messages') ?? "[]")
+
+  constructor(private service: MessageService) {}
+
+  updateLocalStorage(){
+    localStorage.setItem('messages', JSON.stringify(this.messages))
+  }
+
   sendSuggestedQuestion(question: string){
     this.messages.push({
       type: 'request',
       message: question
+    })
+  this.updateLocalStorage() 
+  this.sendMessage(question) 
+
+  }
+  sendMessage(message: string){
+    this.service.sendData(message).subscribe({
+      next: (body) => {
+        this.messages.push({
+          type: 'response',
+          message: body.response
+        })
+      this.updateLocalStorage() 
+      }
     })
   }
 }
