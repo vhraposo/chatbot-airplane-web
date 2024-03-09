a1f7521f437a2473b0747bb1b589d5d7025e27cf
vhraposo <thevhup@gmail.com>
2024-03-08 21:03:53 -0300

    feat: update chat component HTML and TypeScript files

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,19 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ChatDialogComponent } from '../../components/chat-dialog/chat-dialog.component';
 import { ChatInsightsComponent } from '../../components/chat-insights/chat-insights.component';
 import { ArrowLeftComponent } from '../../icons/arrow-left/arrow-left.component';
 import { InterrogationComponent } from '../../icons/interrogation/interrogation.component';
+import { Message } from '../../types/message.type';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
   imports: [
+    CommonModule,
     ArrowLeftComponent,
     InterrogationComponent,
-    ChatInsightsComponent
+    ChatInsightsComponent,
+    ChatDialogComponent
   ],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent {
-  
+  messages: Message[] = []
+  sendSuggestedQuestion(question: string){
+    this.messages.push({
+      type: 'request',
+      message: question
+    })
+  }
 }
