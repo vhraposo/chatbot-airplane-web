da7da4357a6b9c3d7a4fa7391f9aec882ab00918
vhraposo <thevhup@gmail.com>
2024-03-08 21:03:45 -0300

    feat: update chat-response component HTML, SCSS, and TypeScript files

--- src/app/components/chat-response/chat-response.component.ts
+++ src/app/components/chat-response/chat-response.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Message } from '../../types/message.type';
 
 @Component({
-  selector: 'app-chat-response',
+  selector: 'chat-response',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './chat-response.component.html',
   styleUrl: './chat-response.component.scss'
 })
 export class ChatResponseComponent {
-
+  @Input() message!: Message;
 }
